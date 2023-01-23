09e1792ef2d0e8c4811ab52a69563b0adefcb262
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-23 18:05:57 -0500

    add floorplan and kitchen components

---
+++ src/Floorplan.jsx
@@ -0,0 +1,7 @@
+const Floorplan = () => {
+  return (
+    <h1>This is a floorplan component</h1>
+  )
+}
+
+export default Floorplan
---
+++ src/Kitchen.jsx
@@ -0,0 +1,7 @@
+const Kitchen = () => {
+  return (
+    <h2>This is a kitchen component</h2>
+  )
+}
+
+export default Kitchen
