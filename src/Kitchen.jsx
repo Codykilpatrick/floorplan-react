a1bb7bbbde0ae5cc47a3f81cf853b83930ce217f
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-23 18:09:39 -0500

    add sink component

--- src/Kitchen.jsx
+++ src/Kitchen.jsx
@@ -1,10 +1,12 @@
 import Oven from "./Oven"
+import Sink from "./Sink"
 
 const Kitchen = () => {
   return (
     <>
       <h2>This is a kitchen component</h2>
       <Oven />
+      <Sink />
     </>
   )
 }
---
+++ src/Sink.jsx
@@ -0,0 +1,7 @@
+const Sink = () => {
+  return (
+    <h4>This is a sink component</h4>
+  )
+}
+
+export default Sink
